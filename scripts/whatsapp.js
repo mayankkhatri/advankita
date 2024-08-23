19e8f9cc4e024d269ee8439767fa7776d363ccc3
John Doe <mayanksdp11@gmail.com>
2024-08-23 21:38:39 +0530

    mashup services

---
+++ scripts/whatsapp.js
@@ -0,0 +1,10 @@
+//!Whatsapp button
+document.getElementById("whatsappIcon").addEventListener("click", function () {
+  document.getElementById("chatPopup").style.display = "block";
+});
+
+document.getElementById("closeBtn").addEventListener("click", function () {
+  document.getElementById("chatPopup").style.display = "none";
+});
+
+// !Whatsapp button
